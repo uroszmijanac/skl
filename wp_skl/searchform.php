463baad77e4e63111d8ab3b3b465ce6d69ac9d3f
uroszmijanac <urso@trcanje.rs>
2016-03-15 14:37:40 +0100

    obrisao

--- wp_skl/searchform.php
+++
@@ -1,4 +0,0 @@
-					<form class="search-form" method="get" action="<?php echo home_url(); ?>/">
-						<input type="text" placeholder="<?php echo __("Search...", "delicious"); ?>" id="s" name="s" value="<?php the_search_query(); ?>" />
-						<button type="submit" value="Search"><i class="fa fa-search"></i></button>
-					</form>
